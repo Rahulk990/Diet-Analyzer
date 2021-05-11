d5e280087ec1ceb37b5193f735dfff31cecc9fcf
Rahul <rahulv0530@gmail.com>
2021-05-11 17:32:00 +0530

    Done

--- main.js
+++ main.js
@@ -1,30 +1,69 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+const { app, BrowserWindow, Tray, Menu } = require('electron')
 
-function createWindow () {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    // webPreferences: {
-    //   preload: path.join(__dirname, 'preload.js')
-    // }
-  })
+let mainWindow, tray;
+let isQuiting = false;
 
-  win.loadFile('./dist/Diet-Analyzer/index.html')
+function createTray() {
+
+  tray = new Tray('./dist/Diet-Analyzer/assets/food.jpg')
+  tray.setToolTip('Diet Analyzer')
+  tray.on('double-click', () => { mainWindow.show() })
+
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Quit', click: () => {
+        isQuiting = true
+        app.quit()
+      }
+    }
+  ])
+
+  tray.setContextMenu(contextMenu)
 }
 
-app.whenReady().then(() => {
-  createWindow()
+function createWindow() {
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
+  // Basic Methods
+  mainWindow = new BrowserWindow({
+    width: 1000, height: 800,
+    minWidth: 800, minHeight: 600,
+    webPreferences: { nodeIntegration: true },
+    icon: './dist/Diet-Analyzer/assets/food.jpg',
+    title: "Diet Analyzer"
   })
+  mainWindow.loadFile('./dist/Diet-Analyzer/index.html')
+  mainWindow.on('closed', () => { mainWindow = null })
+  mainWindow.webContents.openDevTools();
+
+  // Setting NULL MenuBar
+  mainWindow.setMenu(null);
+
+  // Creating Tray
+  createTray()
+
+  // Configuring Options
+  mainWindow.on('minimize', (event) => {
+    event.preventDefault();
+    mainWindow.hide();
+  });
+
+  mainWindow.on('close', (event) => {
+    if (!isQuiting) {
+      event.preventDefault();
+      mainWindow.hide();
+    }
+    return false;
+  });
+}
+
+app.on('ready', () => {
+  createWindow()
 })
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) createWindow()
 })
